287ed0da9b2c35e180356ddabaae4003fa4133d5
Tri Nguyen <tringuyenminh92@gmail.com>
2016-12-18 18:52:27 +0700

    add app module and main controller, update libs + css

---
+++ PBI/PBI_HandsOnLabs/App_HOL/scripts/custom/HandsOnLabsApp.js
@@ -0,0 +1,25 @@
+﻿
+(function () {
+
+    "use strict";
+
+    angular.module('HandsOnLabsModule', ['ngAnimate', 'ngRoute', 'ui.router', 'ngMaterial']);
+
+    configFunction.$inject = ['$stateProvider', '$httpProvider', '$locationProvider', '$urlRouterProvider', '$mdThemingProvider'];
+    function configFunction($stateProvider, $httpProvider, $locationProvider, $urlRouterProvider, $mdThemingProvider) {
+
+        //$mdThemingProvider.theme('default').primaryPalette('blue', {
+        //    'default': '800',
+        //    'hue-1': '500',
+        //    'hue-2': '600',
+        //    'hue-3': '900'
+        //}).accentPalette('green');
+
+        $locationProvider.html5Mode({ enabled: true, requireBase: false });
+
+        $urlRouterProvider.otherwise('/App_HOL');
+    }
+
+    angular.module('HandsOnLabsModule').config(configFunction);
+
+})();
---
+++ PBI/PBI_HandsOnLabs/App_HOL/scripts/custom/controllers/LandingPageController.js
@@ -0,0 +1,13 @@
+﻿
+function LandingPageController($scope) {
+
+    'use strict';
+
+    $scope.landingPageModel = {
+        title: 'Hands-on-labs'
+    };
+
+   
+}
+LandingPageController.$inject = ['$scope'];
+angular.module('HandsOnLabsModule').controller('LandingPageController', LandingPageController);
